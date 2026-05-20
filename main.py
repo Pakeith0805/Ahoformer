6d26e5381da4ddeeee025409e0285f4ee6a3d510
Katsuhiro Isa <katisa0805@gmail.com>
2026-05-20 13:29:01 +0900

    numpyのテストスクリプト作成

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from Ahoformer!")
+import numpy as np
 
+def main():
+    print("Hello from transformer-scratch!")
+    # 試しにランダムな行列を作ってみる
+    x = np.random.randn(2, 3)
+    print("NumPy Matrix Shape:", x.shape)
 
 if __name__ == "__main__":
     main()
