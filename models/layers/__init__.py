ba4254052e170b104a7d91698ae277752e0327aa
Katsuhiro Isa <katisa0805@gmail.com>
2026-05-25 10:52:22 +0900

    クラスcasual attentionを作成

--- models/layers/__init__.py
+++ models/layers/__init__.py
@@ -1,5 +1,6 @@
 # SelfAttentionを普通に使えるように
 
 from .self_attention import SelfAttention
+from .casual_attention import CasualAttention
 from .ffn import FFN
 from .positional import PositionalEncoding
